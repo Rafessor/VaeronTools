49336cfbc15479c5d5d09b265984f2057a3dcb15
Schuwi <schuwi4@gmail.com>
2017-05-11 15:52:31 +0200

    Version 0.1.1
    vaesweep: Fixed irregular distances on limited-copy spline

--- src/main/java/me/schuwi/vtools/SweepCommand.java
+++ src/main/java/me/schuwi/vtools/SweepCommand.java
@@ -1,6 +1,7 @@
 package me.schuwi.vtools;
 
 import com.boydti.fawe.config.BBC;
+import com.google.common.base.Preconditions;
 import com.sk89q.minecraft.util.commands.CommandPermissions;
 import com.sk89q.worldedit.*;
 import com.sk89q.worldedit.entity.Player;
@@ -17,6 +18,7 @@ import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.util.command.parametric.Optional;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,9 +34,6 @@ public class SweepCommand {
     private static final double quality = 10D;
 
     private static final Vector2D direction = new Vector2D(1, 0);
-    //private static final double scaleConst = Math.sqrt(2) - 1;
-    //private static final Vector direction = new Vector(1, 0, 0);
-    //private static final Vector up = new Vector(0, 1, 0);
 
     @com.sk89q.minecraft.util.commands.Command(
             aliases = { "vaesweep", "/vaesweep" },
@@ -78,13 +77,15 @@ public class SweepCommand {
                 if (copies == 1) {
                     affected += pastePosition(editSession, interpol, holder, baseTransform, 0, offset);
                 } else if (copies > 1) {
+                    List<Section> sections = initSections(interpol, nodes.size(), interpol.arcLength(0, 1));
                     for (double pos = 0; pos <= 1; pos += 1D / (copies - 1)) {
-                        affected += pastePosition(editSession, interpol, holder, baseTransform, pos, offset);
+                        affected += pastePosition(editSession, interpol, holder, baseTransform, flexToUniPosition(sections, pos), offset);
                     }
                 } else {
                     double splineLength = interpol.arcLength(0, 1);
+                    List<Section> sections = initSections(interpol, nodes.size(), splineLength);
                     for (double pos = 0; pos <= 1; pos += 1D / splineLength / quality) {
-                        affected += pastePosition(editSession, interpol, holder, baseTransform, pos, offset);
+                        affected += pastePosition(editSession, interpol, holder, baseTransform, flexToUniPosition(sections, pos), offset);
                     }
                 }
             } catch (MaxChangedBlocksException e) {
@@ -98,6 +99,44 @@ public class SweepCommand {
         } catch (IncompleteRegionException | EmptyClipboardException | ClassCastException ignored) {
             player.print("You have to make a convex polyhedral selection and copy a region before using this command");
         }
+    }
+
+    private List<Section> initSections(Interpolation interpol, int nodeCount, double splineLength) {
+        List<Section> sections = new ArrayList<>();
+        int sectionCount = nodeCount - 1;
+        double sectionLength = 1D / sectionCount;
+
+        double position = 0;
+        for (int i = 0; i < sectionCount; i++) {
+            double length;
+            if (i == sectionCount - 1) { // maybe unnecessary precaution
+                length = interpol.arcLength(i * sectionLength, 1D) / splineLength;
+            } else {
+                length = interpol.arcLength(i * sectionLength, (i + 1) * sectionLength) / splineLength;
+            }
+            sections.add(new Section(i * sectionLength, sectionLength, position, length));
+            position += length;
+        }
+        return Collections.unmodifiableList(sections);
+    }
+
+    private double flexToUniPosition(List<Section> sections, double flexPosition) {
+        Preconditions.checkArgument(flexPosition >= 0);
+        Preconditions.checkArgument(flexPosition <= 1);
+
+        Section previousSection = sections.get(0);
+        for (int i = 1; i < sections.size(); i++) {
+            Section section = sections.get(i);
+            if (flexPosition < section.flexStart) {
+                break;
+            }
+            previousSection = section;
+        }
+
+        double flexOffset = flexPosition - previousSection.flexStart;
+        double uniOffset = flexOffset / previousSection.flexLength * previousSection.uniLength;
+
+        return previousSection.uniStart + uniOffset;
     }
 
     private int pastePosition(EditSession editSession, Interpolation interpol, ClipboardHolder holder, Transform baseTransform, double position, Vector preOffset) throws MaxChangedBlocksException {
@@ -116,7 +155,6 @@ public class SweepCommand {
                 new AffineTransform()
                         .translate(offset)
                         .rotateY(angle)
-                        //.scale(direction.getZ() != 0 ? scale : 1, 1, direction.getX() != 0 ? scale : 1)
                         .coefficients())
                 .preOffset(preOffset);
 
@@ -129,5 +167,19 @@ public class SweepCommand {
                 .build();
         Operations.completeLegacy(operation);
         return ((ForwardExtentCopy)operation).getAffected();
+    }
+
+    private class Section {
+        final double uniStart;
+        final double uniLength;
+        final double flexStart;
+        final double flexLength;
+
+        Section(double uniStart, double uniLength, double flexStart, double flexLength) {
+            this.uniStart = uniStart;
+            this.uniLength = uniLength;
+            this.flexStart = flexStart;
+            this.flexLength = flexLength;
+        }
     }
 }
